3dd79a3efabd8d14ccc513b8277a45ddd8314bce
Oleh Andriienko <OlegAndrienko@gmail.com>
2023-04-25 12:22:12 +0300

    Add files via upload

--- server_v1.py
+++ server_v1.py
@@ -4,12 +4,21 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 import mimetypes
 import json
 import socket
+import logging
+from threading import Thread
 
 
 BASE_DIR = pathlib.Path()
 BUFFER_SIZE = 1024
+SERVER_IP = '127.0.0.1'
+SERVER_PORT =  5000
 
-
+def send_data_to_socket(body):
+    
+    c_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    c_socket.sendto(body, (SERVER_IP, SERVER_PORT))
+    c_socket.close()
+    
 
 class MyHTTPHandler(BaseHTTPRequestHandler):
     
@@ -31,14 +40,11 @@ class MyHTTPHandler(BaseHTTPRequestHandler):
                     self.send_html('C:\\Users\\Oleg\OneDrive\\GOIT_cloud\\web_socket_module_4\\front-init\\error.html', 404)
      
     def do_POST(self):
-        # self.send_html('C:\\Users\\Oleg\\OneDrive\\GOIT_cloud\web_socket_module_4\\front-init\\message.html')
+        # Приймаємо і відправляємо дані сокетом
         
-        body = self.rfile.read(int(self.headers['Content-Length'])) 
-        body = urllib.parse.unquote_plus(body.decode()) # пробирає пробіли
-        payload = {key: value for key, value in [el.split('=') for el in body.split('&')]}
-        with open(BASE_DIR.joinpath('storage/data.json'), 'w', encoding='utf-8') as fd:
-            json.dump(payload, fd, ensure_ascii=False)
-       
+        body = self.rfile.read(int(self.headers['Content-Length']))
+        send_data_to_socket(body)
+       # redirect
         self.send_response(302)
         self.send_header('Location', '/')
         self.end_headers()
@@ -65,30 +71,42 @@ class MyHTTPHandler(BaseHTTPRequestHandler):
             self.wfile.write(f.read())
      
      
-def save_data_from_http_server( data):
+def save_data( data):
     parse_data = urllib.parse.unquote_plus(data.decode())
     dict_parse = {key: value for key, value in [el.split('=' for el in parse_data.split('&'))] }    
         
 
-def run_socket_server(host, port):
+
+
+def run_socket_server(ip, port):
     # host = '127.0.0.1'
     # port = 5000
-
-    s_socket = socket.socket()
-    s_socket.bind((host, port))
-    s_socket.listen(1)
-
-    # conn, addr = s_socket.accept()
-    # print(f"Connection from {addr}")
+    s_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server = ip, port
+    s_socket.bind(server)
+    
     try:
         while True:
-            msg, address = s_socket.recvfrom(BUFFER_SIZE)
-            save_data_from_http_server(msg)
+            data, address = s_socket.recv(BUFFER_SIZE)
+            save_data(data)
     except KeyboardInterrupt:
-        print('socket server stopped')
+        logging.info('socket server stopped')
     finally:     
         s_socket.close()
-    
+        
+
+def save_data(data):
+    body = urllib.parse.unquote_plus(data.decode())
+    try:
+        payload = {key: value for key, value in [
+            el.split('=') for el in body.split('&')]}
+        with open(BASE_DIR.joinpath('storage/data.json'), 'w', encoding='utf-8') as fd:
+            json.dump(payload, fd, ensure_ascii=False)
+    except ValueError as err:
+        logging.error(f"Field parse data {body} with error {err}")
+    except OSError as err:
+        logging.error(f"Field write data {body} with error {err}")
+        
 
 def run(server=HTTPServer, handler=MyHTTPHandler):
     address = ('', 3000)
@@ -99,6 +117,10 @@ def run(server=HTTPServer, handler=MyHTTPHandler):
         http_server.server_close()
             
             
-if __name__ == '__main__':
-   
-    run()
+if __name__ == '__main__':  
+    logging.basicConfig(level=logging.INFO, format="%(threadName)s %(message)s")
+    thread_server = Thread(target=run)
+    thread_server.start()
+    
+    thread_socket=Thread(target=run_socket_server(SERVER_IP, SERVER_PORT))
+    thread_socket.start()
